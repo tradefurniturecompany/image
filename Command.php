4cb62dab4e1039b588aafa9a43ac4847e371d46e
Dmitry Fedyuk <dfediuk@gmail.com>
2020-10-26 01:20:26 +0600

    https://github.com/tradefurniturecompany/image/issues/1

---
+++ Command.php
@@ -0,0 +1,20 @@
+<?php
+namespace TFC\Image;
+# 2020-10-25
+final class Command extends \Df\Framework\Console\Command {
+	/**
+	 * 2020-10-25
+	 * @override
+	 * @see \Symfony\Component\Console\Command\Command::configure()
+	 */
+	protected function configure() {$this->setName('tfc:image')->setDescription('Processes product images');}
+
+	/**
+	 * 2020-10-25
+	 * @override
+	 * @see \Df\Framework\Console\Command::p()
+	 * @used-by \Df\Framework\Console\Command::execute()
+	 * @return void
+	 */
+	protected function p() {$this->output()->writeln(__METHOD__);}
+}
