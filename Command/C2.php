7ef64c507ddad53dfa8f63367d8627b73540ed68
Dmitry Fedyuk <dfediuk@gmail.com>
2020-11-09 19:51:33 +0600

    https://github.com/tradefurniturecompany/image/issues/2

--- Command/C2.php
+++ Command/C2.php
@@ -2,9 +2,11 @@
 namespace TFC\Image\Command;
 use Google\Cloud\Vision\V1\AnnotateImageResponse as Res;
 use Google\Cloud\Vision\V1\BoundingPoly;
+use Google\Cloud\Vision\V1\EntityAnnotation as EA;
 use Google\Cloud\Vision\V1\ImageAnnotatorClient as Annotator;
 use Google\Cloud\Vision\V1\LocalizedObjectAnnotation as O;
-use Google\Cloud\Vision\V1\NormalizedVertex as V;
+use Google\Cloud\Vision\V1\NormalizedVertex as NV;
+use Google\Cloud\Vision\V1\Vertex as V;
 use Google\Protobuf\Internal\RepeatedField;
 use RecursiveDirectoryIterator as RDI;
 use RecursiveIteratorIterator as RII;
@@ -26,88 +28,77 @@ final class C2 extends \Df\Framework\Console\Command {
 	 */
 	protected function p() {
 		df_google_init_service_account();
-		$ii = $this->images(); /** @var string[] $ii */
-		$count = count($ii); /** @var int $count */
-		$c = 0;
-		foreach ($ii as $i) {/** @var string $i */
-			$c++;
-			$this->output()->writeln(sprintf("%d of %d (%d%%): %s", $c, $count, $c * 100 / $count, df_product_image_path2rel($i)));
-			$this->image($i);
-		}
-	}
-
-	/**
-	 * 2020-10-26
-	 * @used-by p()
-	 * @param string $path
-	 * @throws \Google\ApiCore\ApiException
-	 */
-	private function image($path) {
+		$path = dirname(BP) . '/test/1.jpg';
 		$a = new Annotator; /** @var Annotator $a */
 		$f = file_get_contents($path); /** @var string $f */
-		try {$res = $a->objectLocalization($f); /** @var Res $res */}
+		try {$resT = $a->textDetection($f); /** @var Res $resT */}
 		finally {$a->close();}
-		$oo = $res->getLocalizedObjectAnnotations(); /** @var RepeatedField $oo */
-		if (1 === $oo->count()) {
-			# 2020-10-26 https://cloud.google.com/vision/docs/reference/rest/v1/AnnotateImageResponse#LocalizedObjectAnnotation
-			$o = $oo[0]; /** @var O $o */
-			# 2020-10-26
-			# https://cloud.google.com/vision/docs/reference/rest/v1/projects.locations.products.referenceImages#BoundingPoly
-			$bp = $o->getBoundingPoly(); /** @var BoundingPoly $bp */
-			$vv = $bp->getNormalizedVertices(); /** @var RepeatedField $vv */
-			list($w, $h) = getimagesize($path); /** @var int $w */ /** @var int $h */
-			$f = function($a, $b) {return round($a * $b);};
-			$x = function($a) use($f, $w) {return $f($w, $a);};
-			$y = function($a) use($f, $h) {return $f($h, $a);};
-			$v0 = $vv[0]; /** @var V $v0 */
-			$v2 = $vv[2]; /** @var V $v2 */
-			# 2020-10-26
-			# https://cloud.google.com/vision/docs/reference/rest/v1/projects.locations.products.referenceImages#NormalizedVertex
-			$im = imagecreatefromjpeg($path);
-			try {
-				$im2 = imagecrop($im, [
-					'height' => $y($v2->getY() - $v0->getY())
-					,'width' => $x($v2->getX() - $v0->getX())
-					,'x' => $x($v0->getX()), 'y' => $y($v0->getY())
-				]);
-				$path = df_cc_path(dirname(BP), 'result', df_product_image_path2rel($path));
-				if (!is_dir($dir = dirname($path))) {
-					mkdir($dir, 0777, true);
+		$ta = $resT->getTextAnnotations(); /** @var RepeatedField $ta */
+		if ($ta->count()) {
+			$a = new Annotator; /** @var Annotator $a */
+			try {$resO = $a->objectLocalization($f); /** @var Res $resO */}
+			finally {$a->close();}
+			$oo = $resO->getLocalizedObjectAnnotations(); /** @var RepeatedField $oo */
+			if (1 === $oo->count()) {
+				# 2020-10-26
+				# https://cloud.google.com/vision/docs/reference/rest/v1/AnnotateImageResponse#LocalizedObjectAnnotation
+				$o = $oo[0]; /** @var O $o */
+				$t = $ta[0]; /** @var EA $t */
+				# 2020-10-26
+				# https://cloud.google.com/vision/docs/reference/rest/v1/projects.locations.products.referenceImages#BoundingPoly
+				$bpT = $t->getBoundingPoly(); /** @var BoundingPoly $bpT */
+				$vvT = $bpT->getVertices(); /** @var RepeatedField $vvT */
+				$bpO = $o->getBoundingPoly(); /** @var BoundingPoly $bpO */
+				$vvO = $bpO->getNormalizedVertices(); /** @var RepeatedField $vvO */
+				list($w, $h) = getimagesize($path); /** @var int $w */ /** @var int $h */
+				$f = function($a, $b) {return round($a * $b);};
+				$x = function($a) use($f, $w) {return $f($w, $a);};
+				$y = function($a) use($f, $h) {return $f($h, $a);};
+				$n2v = function(NV $v) use($x, $y) {
+					$r = new V;
+					$r->setX($x($v->getX()));
+					$r->setY($y($v->getY()));
+					return $r;
+				};
+				$v0O = $n2v($vvO[0]); /** @var V $v0O */
+				$v2O = $n2v($vvO[2]); /** @var V $v2O */
+				$pad = function(V $v0, V $v2) use($w, $h) {
+					$d = 10;
+					$v0->setX(max(0, $v0->getX() - $d));
+					$v0->setY(max(0, $v0->getY() - $d));
+					$v2->setX(min($w, $v2->getX() + $d));
+					$v2->setY(min($h, $v2->getY() + $d));
+					return [];
+				};
+				$v0T = $vvT[0]; /** @var V $v0T */
+				$v2T = $vvT[2]; /** @var V $v2T */
+				$pad($v0T, $v2T);
+				$union = function(V $a0, V $a2, V $b0, V $b2) {
+					$r0 = new V; /** @var V $r0 */
+					$r2 = new V; /** @var V $r2 */
+					$r0->setX(min($a0->getX(), $b0->getX()));
+					$r0->setY(min($a0->getY(), $b0->getY()));
+					$r2->setX(max($a2->getX(), $b2->getX()));
+					$r2->setY(max($a2->getY(), $b2->getY()));
+					return [$r0, $r2];
+				};
+				list($v0, $v2) = $union($v0O, $v2O, $v0T, $v2T);
+				$im = imagecreatefromjpeg($path);
+				try {
+					$im2 = imagecrop($im, [
+						'height' => $v2->getY() - $v0->getY()
+						,'width' => $v2->getX() - $v0->getX()
+						,'x' => $v0->getX(), 'y' => $v0->getY()
+					]);
+					$path = df_cc_path(dirname(BP), 'result', basename($path));
+					if (!is_dir($dir = dirname($path))) {
+						mkdir($dir, 0777, true);
+					}
+					imagejpeg($im2, $path);
+					imagedestroy($im2);
 				}
-				imagejpeg($im2, $path);
-				imagedestroy($im2);
-			}
-			finally {imagedestroy($im);}
-		}
-	}
-
-	/**
-	 * 2020-10-26
-	 * @used-by p()
-	 * @return string[]
-	 */
-	private function images() {return dfc($this, function() {return $this->scan(
-		df_product_images_path(), ['cache', 'placeholder']
-	);});}
-
-	/**
-	 * 2020-10-26
-	 * @used-by images()
-	 * @used-by scan()
-	 * @param string $base
-	 * @param string[] $skip [optional]
-	 * @return string[]
-	 */
-	private function scan($base, $skip = []) {
-		$r = []; /** @var string[] $r */
-		foreach (array_diff(scandir($base = "$base/"), array_merge($skip, ['.', '..'])) as $f) {
-			if (is_dir($f = $base . $f)) {
-				$r = array_merge($r, $this->scan($f));
-			}
-			else if (in_array(strtolower(df_file_ext($f)), ['jpg', 'jpeg'])) {
-				$r[]= $f;
+				finally {imagedestroy($im);}
 			}
 		}
-		return $r;
 	}
 }
