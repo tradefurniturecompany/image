7ec95710dc88a108380461a53c76758da6ab0189
Dmitry Fedyuk <dfediuk@gmail.com>
2020-11-22 08:33:50 +0600

    https://github.com/tradefurniturecompany/image/issues/2

--- Command/C3.php
+++ Command/C3.php
@@ -1,42 +1,79 @@
 <?php
 namespace TFC\Image\Command;
-use Google\ApiCore\ApiException as AE;
-use Google\Cloud\Vision\V1\BoundingPoly;
-use Google\Cloud\Vision\V1\EntityAnnotation as EA;
-use Google\Cloud\Vision\V1\LocalizedObjectAnnotation as O;
-use Google\Cloud\Vision\V1\NormalizedVertex as NV;
-use Google\Cloud\Vision\V1\Vertex as V;
 use Google\Protobuf\Internal\RepeatedField;
-use Symfony\Component\Console\Input\InputArgument as Arg;
-use Symfony\Component\Console\Input\InputOption as Opt;
+use Magento\Catalog\Model\Product as P;
+use Magento\Catalog\Model\ResourceModel\Product\Collection as PC;
+use Magento\Framework\DataObject as _DO;
+use Magento\Framework\Exception\LocalizedException as LE;
 use TFC\Image\I;
-# 2020-11-09 "Images with a grey text are incorrectly cropped": https://github.com/tradefurniturecompany/image/issues/2
+# 2020-11-22 "Images with a grey text are incorrectly cropped": https://github.com/tradefurniturecompany/image/issues/2
 final class C3 extends \Df\Framework\Console\Command {
 	/**
-	 * 2020-10-25
+	 * 2020-11-22
 	 * @override
 	 * @see \Symfony\Component\Console\Command\Command::configure()
 	 */
-	protected function configure() {
-		$this->setName('tfc:image:3')->setDescription('Processes product images');
-		$this->setDefinition([new Opt(self::$P_CATEGORY, null, Arg::OPTIONAL)]);
-	}
+	protected function configure() {$this->setName('tfc:image:3')->setDescription('Processes product images');}
 
 	/**
-	 * 2020-10-25
+	 * 2020-11-22
 	 * @override
 	 * @see \Df\Framework\Console\Command::p()
 	 * @used-by \Df\Framework\Console\Command::execute()
-	 * @throws AE
+	 * @throws LE
 	 */
 	protected function p() {
-		$this->output()->writeln(df_product_c()->count());
+		$conn = '2020_07_04';
+		df_google_init_service_account();
+		$base = df_product_images_path(); /** @var string $base */
+		$baseL = df_cc_path(dfa(df_conn($conn)->getConfig(), 'path'), df_product_images_path_rel()); /** @var string $baseL */
+		foreach ($this->pcL() as $pL) {/** @var P $pL */
+			if ($i = $this->findLabeled($pL, $baseL)) {/** @var I $i */
+				/** @var P $p */
+				if (($p = $this->pc()->getItemById($pL->getId())) && !$this->findLabeled($p, $base)) {
+					$this->output()->writeln("Labeled: {$i->basename()}");
+				}
+			}
+		}
 	}
 
 	/**
-	 * 2020-10-11
-	 * @used-by configure()
+	 * 2020-11-22
+	 * @param P $p
+	 * @param string $base
+	 * @return I|null
+	 */
+	private function findLabeled(P $p, $base) {return df_find(function(_DO $io) use($base) {
+		$i = new I($base . $io['file']); /** @var I $i */
+		return $i->annotationsText()->count() ? $i : null;
+	}, $p->getMediaGalleryImages());}
+
+	/**
+	 * 2020-11-22
+	 * @return PC
+	 */
+	private function pc() {return dfc($this, function() {return $this->pcPrepare(df_product_c());});}
+
+	/**
+	 * 2020-11-22
+	 * @return PC
+	 */
+	private function pcL() {return dfc($this, function() {return df_with_conn(self::$CONN_L, function() {return $this->pcPrepare(
+		df_product_c()->setPageSize(1)->addFieldToFilter('entity_id', 116)
+	);});});}
+
+	/**
+	 * 2020-11-22 https://magento.stackexchange.com/a/228181
+	 * @used-by pc()
+	 * @used-by pcL()
+	 * @param PC $c
+	 * @return PC
+	 */
+	private function pcPrepare(PC $c) {return $c->addMediaGalleryData();}
+
+	/**
+	 * 2020-11-22
 	 * @var string
 	 */
-	private static $P_CATEGORY = 'category';
+	private static $CONN_L = '2020_07_04';
 }
