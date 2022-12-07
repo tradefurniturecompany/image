f97535b790399f3f1fef4e7375b0e264f7acb39b
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-07 13:03:03 +0000

    refactoring

--- Command/C3.php
+++ Command/C3.php
@@ -3,7 +3,6 @@ namespace TFC\Image\Command;
 use Magento\Catalog\Model\Product as P;
 use Magento\Catalog\Model\ResourceModel\Product\Collection as PC;
 use Magento\Framework\DataObject as _DO;
-use Magento\Framework\Exception\LocalizedException as LE;
 use TFC\Image\I;
 # 2020-11-22 "Images with a grey text are incorrectly cropped": https://github.com/tradefurniturecompany/image/issues/2
 final class C3 extends \Df\Framework\Console\Command {
